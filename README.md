6cd5b5bcd5e6ab3cf8fec8ae8de378252838b57a
katemcg <katemcg@umich.edu>
2023-05-10 10:00:18 -0400

    updates to ReadMe and making time_diffs to np array Kate
    Former-commit-id: d25651cc69f34ce74af48144b6be7bf7a3080187

--- README.md
+++ README.md
@@ -2,11 +2,31 @@
 
 A repository for a Python suite of neutron noise analysis algorithms. This draws inspiration from faust lmx on gitlab.lanl.gov. This suite is designed specifically for pulse time-of-detection chains from organic scintillator arrays.
 
-# Sample GitLab Project
+## Rossi-Alpha Method
 
-This sample project shows how a project in GitLab looks for demonstration purposes. It contains issues, merge requests and Markdown files in many branches,
-named and filled with lorem ipsum.
+### Setup
+You will need to install python and then install numpy. 
+To begin, you must create a local repo. To do this, initialize the local repo by typing the following command into your terminal:
+$ git init
+This will create a .git subdirectory which will contain files for the repo.
+To check the status of the repo at any point, type: 
+$ git status
+To clone the project onto your machine locally, type
+$ git clone https://gitlab.eecs.umich.edu/umich-dnng/pynoise.git
 
-You can look around to get an idea how to structure your project and, when done, you can safely delete this project.
 
-[Learn more about creating GitLab projects.](https://docs.gitlab.com/ee/gitlab-basics/create-project.html)
+### How to Use
+The files that you will need in order to use the Rossi-Alpha program are fitting.py, plots.py, timeDifs.py, Rossi_alpha_settings.txt, and a file that you want to analyze. The format of the file you want to analyze should be a txt file with a list of time stamps of neutron detection times, separated by new lines. The format of the Rossi_alpha_settings.txt should be a list of settings:
+*reset time
+*bin width
+*minimum cutoff
+*fit range
+*plot scale
+*time difference method
+*digital delay
+*number of folders
+*meas time per folder
+Users can edit this .txt file to change the settings that they wish to use.
+INSERT DESCRIPTION OF DIFFERENT SETTINGS. 
+
+
--- timeDifs.py
+++ timeDifs.py
@@ -1,6 +1,6 @@
-import numpy as np                    # For processing data
+import numpy as np  # For processing data
 
-'''
+"""
 Rossi_alpha_settings = dict([('reset time', 2e3), ('bin width', 2),
                              ('minimum cutoff', 25), ('fit range', [0, 2e3]),
                              ('plot scale', 'linear'), 
@@ -8,82 +8,90 @@ Rossi_alpha_settings = dict([('reset time', 2e3), ('bin width', 2),
                               'any-and-all'), ('digital delay', 750),
                              ('number of folders', 61),
                              ('meas time per folder', 60)])
-'''
-
+"""
 
 
 def calculate_time_differences(list_data, Rossi_alpha_settings):
-    
     time_vector = list_data
-    
-    reset_time = float(Rossi_alpha_settings['reset time'])
-    
-    if Rossi_alpha_settings['time difference method'] == 'any_and_all':
-        time_diffs = any_and_all_time_differences(time_vector,reset_time)
-    elif (Rossi_alpha_settings['time difference method'] == 
-          'any-and-all cross-correlations'):
-        channels = list_data[:,0]
+
+    reset_time = float(Rossi_alpha_settings["reset time"])
+
+    if Rossi_alpha_settings["time difference method"] == "any_and_all":
+        time_diffs = any_and_all_time_differences(time_vector, reset_time)
+    elif (
+        Rossi_alpha_settings["time difference method"]
+        == "any-and-all cross-correlations"
+    ):
+        channels = list_data[:, 0]
         time_diffs = any_and_all_cross_correlation_time_differences(
-                        time_vector,channels,reset_time)
-   
-    elif (Rossi_alpha_settings['time difference method'] == 
-          'any-and-all cross-correlations no-repeat'):
-        channels = list_data[:,0]
+            time_vector, channels, reset_time
+        )
+
+    elif (
+        Rossi_alpha_settings["time difference method"]
+        == "any-and-all cross-correlations no-repeat"
+    ):
+        channels = list_data[:, 0]
         time_diffs = any_and_all_cross_correlation_no_repeat_time_differences(
-                        time_vector,channels,reset_time)
-        
-    elif (Rossi_alpha_settings['time difference method'] == 
-          'any-and-all cross-correlations no-repeat digital-delay'):
-        channels = list_data[:,0]
-        time_diffs = \
+            time_vector, channels, reset_time
+        )
+
+    elif (
+        Rossi_alpha_settings["time difference method"]
+        == "any-and-all cross-correlations no-repeat digital-delay"
+    ):
+        channels = list_data[:, 0]
+        time_diffs = (
             any_and_all_cross_correlation_no_repeat_digital_delay_time_differences(
-                        time_vector,channels,reset_time)
+                time_vector, channels, reset_time
+            )
+        )
     else:
         print()
-        print(Rossi_alpha_settings['time difference method'])
-        print('This time difference method requested is either mistyped or')
-        print('has not been programmed. The current available options are:')
+        print(Rossi_alpha_settings["time difference method"])
+        print("This time difference method requested is either mistyped or")
+        print("has not been programmed. The current available options are:")
         print()
-        print('any-and-all')
-        print('any-and-all cross-correlations')
-        print('any-and-all cross-correlations no-repeat')
-        print('any-and-all cross-correlations no-repeat digital-delay')
+        print("any-and-all")
+        print("any-and-all cross-correlations")
+        print("any-and-all cross-correlations no-repeat")
+        print("any-and-all cross-correlations no-repeat digital-delay")
         print()
-    
+
     return time_diffs
 
-def any_and_all_time_differences(time_vector,reset_time):
-    time_diffs = []
+
+def any_and_all_time_differences(time_vector, reset_time):
+    time_diffs = np.array([])
     n = len(time_vector)
     for i in range(n):
-        for j in range(i+1, n):
+        for j in range(i + 1, n):
             if time_vector[j] - time_vector[i] > reset_time:
                 break
             time_diffs.append(time_vector[j] - time_vector[i])
-            
+
     return time_diffs
 
 
-
-
-def any_and_all_cross_correlation_time_differences(
-        time_vector,channels,reset_time):
-    time_diffs = []
+def any_and_all_cross_correlation_time_differences(time_vector, channels, reset_time):
+    time_diffs = np.array([])
     n = len(time_vector)
     for i in range(n):
-        for j in range(i+1, n):
-            if channels[j] != channels[i] :
+        for j in range(i + 1, n):
+            if channels[j] != channels[i]:
                 if time_vector[j] - time_vector[i] > reset_time:
                     break
                 time_diffs.append(time_vector[j] - time_vector[i])
     return time_diffs
 
+
 def any_and_all_cross_correlation_no_repeat_time_differences(
-        time_vector,channels,reset_time):
-    time_diffs = []
+    time_vector, channels, reset_time
+):
+    time_diffs = np.array([])
     for i in range(len(time_vector)):
         ch_bank = []
-        for j in range(i+1, len(time_vector)):
+        for j in range(i + 1, len(time_vector)):
             if channels[j] - channels[i] != 0:
                 if time_vector[j] - time_vector[i] > reset_time:
                     break
@@ -94,13 +102,14 @@ def any_and_all_cross_correlation_no_repeat_time_differences(
 
 
 def any_and_all_cross_correlation_no_repeat_digital_delay_time_differences(
-        time_vector,channels,reset_time):
-    digital_delay = Rossi_alpha_settings['digital delay']
-    time_diffs = []
+    time_vector, channels, reset_time, Rossi_alpha_settings
+):
+    digital_delay = Rossi_alpha_settings["digital delay"]
+    time_diffs = np.array([])
     i = 0
     while i < len(time_vector):
         ch_bank = []
-        for j in range(i+1, len(time_vector)):
+        for j in range(i + 1, len(time_vector)):
             if channels[j] - channels[i] != 0:
                 if time_vector[j] - time_vector[i] > reset_time:
                     break
