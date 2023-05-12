f8c3c6f44603527d2b122f886c96c6c0004a7ec8
Vincent Weng <vincentweng@Vincents-MacBook-Pro.local>
2023-05-12 14:51:54 -0400

    commenting
    Former-commit-id: a2f31421a45a900c5fcba6060e5f534ba6e5e373

--- analyzingFolders.py
+++ analyzingFolders.py
@@ -1,6 +1,9 @@
 
 import os  # For scanning directories
 import numpy as np
+from timeDifs import timeDifCalcs
+from plots import Plot
+import fitting
 #--------------------------------------------------------------------------------
 
 def compile_sample_stdev_RA_dist(data_folder, general_settings):
@@ -17,17 +20,34 @@ def compile_sample_stdev_RA_dist(data_folder, general_settings):
                 list_data_n = np.loadtxt(path_to_data)
                 list_data_n = list_data_n[:,1]
                 from timeDifs import timeDifCalcs
-                thisData = timeDifCalcs(list_data_n, general_settings) 
+                thisData = timeDifCalcs(list_data_n, general_settings)
+                # hist_settings = ________  TODO
+                # line_settings = ________ TODO
+                # residual settings = _________ TODO
+                
                 if i == 0:
                     time_diffs = thisData.calculate_time_differences()
                     [RA_hist, popt, pcov, x_fit, y_fit] = make_RA_hist_and_fit(time_diffs, general_settings)
                     RA_hist_array = RA_hist[0]
                     popt_array = popt
+
+                    # thisPlot = Plot(theseSettings.general_program_settings, theseSettings.histogram_settings) TODO
+                    # counts, bin_centers = thisPlot.plot(time_diffs)
+
+                    # fitting.fit_and_residual(counts, bin_centers, theseSettings.general_program_settings['minimum cutoff'], "Time Differences (ns)", 
+                             # "Coincidence rate (s^-1)", "Any-and-all", theseSettings.line_fitting_settings, theseSettings.residual_plot_settings)
+
                 else:
                     time_diffs = thisData.calculate_time_differences()
                     [RA_hist, popt, pcov, x_fit, y_fit] = make_RA_hist_and_fit(time_diffs, Rossi_alpha_settings)
                     RA_hist_array = np.vstack((RA_hist_array, RA_hist[0]))
                     popt_array = np.vstack((popt_array, popt))
+
+                    # thisPlot = Plot(theseSettings.general_program_settings, theseSettings.histogram_settings) TODO
+                    # counts, bin_centers = thisPlot.plot(time_diffs)
+
+                    # fitting.fit_and_residual(counts, bin_centers, theseSettings.general_program_settings['minimum cutoff'], "Time Differences (ns)", 
+                             # "Coincidence rate (s^-1)", "Any-and-all", theseSettings.line_fitting_settings, theseSettings.residual_plot_settings)
                     
                 i = i + 1
                 
