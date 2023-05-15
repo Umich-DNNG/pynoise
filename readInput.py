a4442033c266db0342970d45af2ba24bccd028aa
katemcg <katemcg@umich.edu>
2023-05-15 11:31:08 -0400

    fixed the range
    Former-commit-id: 54c7fed528849fdd202f60b85585bc4b7e2410fe

--- readInput.py
+++ readInput.py
@@ -1,18 +1,27 @@
-
 import os  # For scanning directories
+import numpy as np
 
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
-#settings class stores the separate dictionaries of settings created from the input file
+
+# settings class stores the separate dictionaries of settings created from the input file
 class Settings:
-    def __init__(self,io_file_info, general_program_settings,histogram_settings,line_fitting_settings,residual_plot_settings):
+    def __init__(
+        self,
+        io_file_info,
+        general_program_settings,
+        histogram_settings,
+        line_fitting_settings,
+        residual_plot_settings,
+    ):
         self.io_file_info = io_file_info
         self.general_program_settings = general_program_settings
         self.histogram_settings = histogram_settings
         self.line_fitting_settings = line_fitting_settings
         self.residual_plot_settings = residual_plot_settings
 
-#reads in the input from settings.txt and stores it in dictionaries
+
+# reads in the input from settings.txt and stores it in dictionaries
 def readInput():
     current_path = os.path.realpath(__file__)
     file_path = os.path.join(os.path.dirname(current_path), "settings.txt")
@@ -21,7 +30,6 @@ def readInput():
     histogram_settings = {}
     line_fitting_settings = {}
     residual_plot_settings = {}
-
 
     with open(file_path, "r") as f:
         current_section = None
@@ -41,13 +49,25 @@ def readInput():
             else:
                 settingName, setting = line.strip().split(":")
                 setting = setting.strip()
-                if settingName == "fit range":
-                    setting = [float(s) for s in setting[1:-1].split(",")]
+                if settingName == "fit range ":
+                    setting_array = np.array([float(d) for d in setting.strip("[]").split(",")])
+                    current_section[settingName.strip()] = setting_array
                 elif setting.isdigit():
                     setting = float(setting)
-                current_section[settingName.strip()] = setting
-        general_program_settings["reset time"] = float(general_program_settings["reset time"])
-        general_program_settings["bin width"] = int(general_program_settings["bin width"])
-        theseSettings = Settings(io_file_info,general_program_settings,histogram_settings,line_fitting_settings,residual_plot_settings)
+                    current_section[settingName.strip()] = setting
+                else:
+                    current_section[settingName.strip()] = setting
+        general_program_settings["reset time"] = float(
+            general_program_settings["reset time"]
+        )
+        general_program_settings["bin width"] = int(
+            general_program_settings["bin width"]
+        )
+        theseSettings = Settings(
+            io_file_info,
+            general_program_settings,
+            histogram_settings,
+            line_fitting_settings,
+            residual_plot_settings,
+        )
     return theseSettings
-
