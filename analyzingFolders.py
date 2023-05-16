f1d4e77b7fac99c439327690785969b0c64b69fb
katemcg <katemcg@umich.edu>
2023-05-16 15:56:39 -0400

    sped up the process for finding the file and fixed the issue with 0s
    Former-commit-id: e2e155c3476f79723359dca05b6eb955707c129f

--- analyzingFolders.py
+++ analyzingFolders.py
@@ -6,16 +6,24 @@ import fitting
 import matplotlib.pyplot as plt  # For plotting data summaries
 
 # --------------------------------------------------------------------------------
+def replace_zeroes(lst):
+    non_zero_elements = [x for x in lst if x != 0]
+    average = sum(non_zero_elements) / len(non_zero_elements)
 
+    for i in range(len(lst)):
+        if lst[i] == 0:
+            lst[i] = average
+
+    return lst
 
 def compile_sample_stdev_RA_dist(settings):
     data_folder = settings.io_file_info["input folder"]
-    num_folders = 10
+    num_folders = int(settings.general_program_settings['number of folders'])
     i = 0
     for fol_num in range(1, num_folders + 1):
-        for filename in os.listdir(data_folder + "/" + str(fol_num)):
-            if filename.endswith("n_allch.txt"):
-                path_to_data = data_folder + "/" + str(fol_num) + "/" + filename
+        #for filename in os.listdir(data_folder + "/" + str(fol_num)):
+            #if filename.endswith("n_allch.txt"):
+                path_to_data = data_folder + "/" + str(fol_num) + "/" + "board0_n_allch.txt"
                 list_data_n = np.loadtxt(path_to_data, delimiter=" ")
                 # list_data_n = np.genfromtxt(path_to_data, delimiter=' ')
                 list_data = list_data_n[:, 1]
@@ -28,9 +36,7 @@ def compile_sample_stdev_RA_dist(settings):
 
                 if i == 0:
                     time_diffs = thisData.calculate_time_differences()
-                    # [RA_hist, popt, pcov, x_fit, y_fit] = make_RA_hist_and_fit(time_diffs, general_settings)
-                    # RA_hist_array = RA_hist[0]
-                    # popt_array = popt
+
 
                     thisPlot = Plot(
                         settings.generating_histogram_settings,
@@ -39,7 +45,7 @@ def compile_sample_stdev_RA_dist(settings):
                     )
                     counts, bin_centers, bin_edges = thisPlot.plot(time_diffs)
                     RA_hist_array = counts
-                    #popt = fitting.fit_and_residual(counts,bin_centers,settings.general_program_settings["minimum cutoff"],settings.general_program_settings["fit range"],"Time Differences (ns)","Coincidence rate (s^-1)","Any-and-all",settings.line_fitting_settings,settings.residual_plot_settings,False,)
+
                     from fitting import Fit
                     thisFit = Fit(counts,bin_centers,settings.generating_histogram_settings,settings.line_fitting_settings, settings.general_program_settings,settings.residual_plot_settings,False)
                     popt = thisFit.fit_and_residual
@@ -47,9 +53,7 @@ def compile_sample_stdev_RA_dist(settings):
 
                 else:
                     time_diffs = thisData.calculate_time_differences()
-                    # [RA_hist, popt, pcov, x_fit, y_fit] = make_RA_hist_and_fit(time_diffs, Rossi_alpha_settings)
-                    # RA_hist_array = np.vstack((RA_hist_array, RA_hist[0]))
-                    # popt_array = np.vstack((popt_array, popt))
+
 
                     thisPlot = Plot(
                         settings.generating_histogram_settings,
@@ -64,13 +68,15 @@ def compile_sample_stdev_RA_dist(settings):
                     popt_array = np.vstack((popt_array, popt))
 
                 i = i + 1
-                break
+                #break
 
 
     RA_std_dev = np.std(RA_hist_array, axis=0, ddof=1)
     RA_hist_total = np.sum(RA_hist_array, axis=0)
 
     time_diff_centers = bin_edges[1:] - np.diff(bin_edges[:2]) / 2
-    RA_hist_total = np.vstack((RA_hist_total, time_diff_centers, RA_std_dev * num_folders))
-
+    uncertainties = RA_std_dev * num_folders
+    uncertainties = replace_zeroes(uncertainties)
+    RA_hist_total = np.vstack((RA_hist_total, time_diff_centers,uncertainties))
+     
     return RA_hist_total
