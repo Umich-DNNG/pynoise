9c874742340e4a16e78bd1349906dbcba6a28ad5
Vincent Weng <vincentweng@Vincents-MacBook-Pro.local>
2023-05-18 10:32:20 -0400

    fitting.py and plots.py comments
    Former-commit-id: dcc5c3c71997d937a601cabe901ba3519f792f4b

--- RossiAlpha/plots.py
+++ RossiAlpha/plots.py
@@ -3,18 +3,29 @@ import matplotlib.pyplot as plt  # For plotting data summaries
 from matplotlib.colors import LogNorm  # For adjusting colorbar scale
 import copy
 
-
 # --------------------------------------------------------------------------------
 # OWN CODE BELOW
 # --------------------------------------------------------------------------------
 class Plot:
     def __init__(self, plotSettings, plotOptions, show_plot):
 
-        # plotting options
+        '''
+        Creating the a Plot() object and its variables.
+
+        Inputs:
+            - plotSettings (setting for the plotting)
+            - plotOptions (style setting for different plotting options)
+            - show_plot (boolean to show or not to show plots)
+
+        Outputs: 
+            - Plot() object
+        '''
+
+        # Plotting options
         self.show_plot = show_plot
         self.options = plotOptions
 
-        # required parameters
+        # Required parameters
         self.reset_time = plotSettings["reset time"]
         self.bin_width = plotSettings["bin width"]
         self.x_axis = "Time Differences"
@@ -23,41 +34,51 @@ class Plot:
         
 
     def plot(self, time_diffs):
-        # calculating number of bins for histogram plot
+
+        '''
+        Creating histogram from an array of time differences and plotting it.
+        Saving and showing the plot can be turned on or off.
+
+        Inputs:
+            - self (all the private variables in Plot() object)
+            - time_diffs (array of time differences)
+
+        Outputs: 
+            - counts (The set of values of the histogram as a list)
+            - bin_centers (adjusted bin centers for visual plotting)
+            - bin_edges (The edges of the bins are mentioned as a parameter)
+        '''
+
+        # Calculating the number of bins
         num_bins = int(self.reset_time / self.bin_width)
 
-        # generating histogram
-        counts, bin_edges = np.histogram(
-            time_diffs, bins=num_bins, range=[0, self.reset_time])
-        
-        # adjusting the bin centers
+        # Generating histogram
+        counts, bin_edges = np.histogram(time_diffs, bins=num_bins, range=[0, self.reset_time])
+
+        # Adjusting the bin centers
         bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
 
-        if self.show_plot == True:
-            # adjusting the bin centers
-            bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
-
-            # plotting
-            plt.plot(bin_centers, counts, drawstyle='steps-post', **self.options)
-            plt.bar(
-                bin_centers,
-                counts,
+        # Plotting
+        plt.plot(bin_centers, counts, drawstyle='steps-post', **self.options)
+        plt.bar(bin_centers, counts,
                 width=0.8 * (bin_centers[1] - bin_centers[0]),
                 alpha=0.6,
                 color="b",
                 align="center",
                 edgecolor="k",
                 linewidth=0.5,
-                fill=True,
-            )
-            plt.xlabel(self.x_axis)
-            plt.ylabel(self.y_axis)
-            plt.title(self.title)
+                fill=True)
+        
+        plt.xlabel(self.x_axis)
+        plt.ylabel(self.y_axis)
+        plt.title(self.title)
+
+        # Showing plot (optional)
+        if self.show_plot == True:
             plt.show()
 
-        # saving plot
+        # Saving plot (optional)
         if self.show_plot == True:
             plt.savefig('histogram', dpi=300, bbox_inches='tight')
 
-            
         return counts, bin_centers, bin_edges
