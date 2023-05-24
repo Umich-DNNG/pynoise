0990a77f3051169c6cf47aff80c2eaaf1cbe4f38
maxwten <maxwten@umich.edu>
2023-05-24 12:17:46 -0400

    Added plot settings adding
    Former-commit-id: 2183798da18ee8c24c01b3ec3d74d9a4beffb215

--- RossiAlpha/settings.py
+++ RossiAlpha/settings.py
@@ -2,111 +2,76 @@ class Settings:
 
     def __init__(self,
                  
-                 ioSettings={   'input':'',
-                                'output':'',
-                                'type':2},
+                 ioSettings={   'Input file/folder':'',
+                                'Output file':'',
+                                'Input type':2},
 
-                 genSettings={  'fit':[0,2e3],
-                                'scale':'linear',
-                                'method':'any_and_all',
-                                'delay':750,
-                                'num':10,
-                                'meas':60,
-                                'sort':'yes',
-                                'save':'yes',
-                                'show':'no',
-                                'dir':'./'},
+                 genSettings={  'Fit range':[0,2e3],
+                                'Plot scale':'linear',
+                                'Time difference method':'any_and_all',
+                                'Digital Delay':750,
+                                'Number of folders':10,
+                                'Meas time per folder':60,
+                                'Sort data?':'yes',
+                                'Save figures?':'yes',
+                                'Show plots?':'no',
+                                'Save directory':'./'},
 
                 visSettings={   'color':'black',
-                                'edge':'blue',
-                                'face':'black',
-                                'style':'-',
-                                'width':3},
+                                'markeredgecolor':'blue',
+                                'markerfacecolor':'black',
+                                'linestyle':'-',
+                                'linewidth':3},
 
-                histSettings={  'time':2e3,
-                                'width':2,
-                                'cutoff':30},
+                histSettings={  'Reset time':2e3,
+                                'Bin width':2,
+                                'Minimum cutoff':30},
 
                 fitSettings={   'color':'red',
-                                'edge':'blue',
-                                'face':'black',
-                                'style':'-',
-                                'width':1},
+                                'markeredgecolor':'blue',
+                                'markerfacecolor':'black',
+                                'linestyle':'-',
+                                'linewidth':1},
 
                 resSettings={'s':8.0}
                 ):
         
-        self.settings = {'io_file_info': ioSettings,
-                         'general_program_settings': genSettings,
-                         'histogram_visual_settings': visSettings,
-                         'generating_histogram_settings': histSettings,
-                         'line_fitting_settings': fitSettings,
-                         'residual_plot_settings': resSettings
+        self.settings = {'Input/Output Settings': ioSettings,
+                         'General Settings': genSettings,
+                         'Histogram Visual Settings': visSettings,
+                         'Histogram Generation Settings': histSettings,
+                         'Line Fitting Settings': fitSettings,
+                         'Residual Plot Settings': resSettings
         }
 
     def set(self, type, setting, value):
         self.settings[type][setting] = value
     
     def get(self, type, setting):
-        return self.settings[type][setting]
+        return self.settings[type].get(setting)
+    
+    def remove(self, type, setting):
+        self.settings[type].pop(setting)
 
-    def print_io(self):
-        print('INPUT/OUTPUT SETTINGS')
-        print('Input type:',self.get('io_file_info','type'))
-        print('Input file/folder:',self.get('io_file_info','input'))
-        print('Output file:',self.get('io_file_info','output'))
-
-    def print_gen(self):
-        print('GENERAL SETTINGS')
-        print('Fit range:', self.settings['general_program_settings']['fit'][0],
-                       'to', self.settings['general_program_settings']['fit'][1])
-        print('Plot scale:',self.settings['general_program_settings']['scale'])
-        print('Time difference method:',self.settings['general_program_settings']['method'])
-        print('Digital Delay',self.settings['general_program_settings']['delay'])
-        print('Number of folders:',self.settings['general_program_settings']['num'])
-        print('Meas time per folder:',self.settings['general_program_settings']['meas'])
-        print('Sort data?:',self.settings['general_program_settings']['sort'])
-        print('Save figures?:',self.settings['general_program_settings']['save'])
-        print('Show plots?:',self.settings['general_program_settings']['show'])
-        print('Save directory:',self.settings['general_program_settings']['dir'])
-
-    def print_vis(self):
-        print('HISTOGRAM VISUAL SETTINGS')
-        print('Main color:',self.settings['histogram_visual_settings']['color'])
-        print('Edge color:',self.settings['histogram_visual_settings']['edge'])
-        print('Face color:',self.settings['histogram_visual_settings']['face'])
-        print('Line style:',self.settings['histogram_visual_settings']['style'])
-        print('Line width:',self.settings['histogram_visual_settings']['width'])
-
-    def print_hist(self):
-        print('HISTOGRAM GENERATION SETTINGS')
-        print('Reset time:',self.settings['generating_histogram_settings']['time'])
-        print('Bin width:',self.settings['generating_histogram_settings']['width'])
-        print('Minimum cutoff:',self.settings['generating_histogram_settings']['cutoff'])
-
-    def print_fit(self):
-        print('LINE FITTING SETTINGS')
-        print('Main color:',self.settings['line_fitting_settings']['color'])
-        print('Edge color:',self.settings['line_fitting_settings']['edge'])
-        print('Face color:',self.settings['line_fitting_settings']['face'])
-        print('Line style:',self.settings['line_fitting_settings']['style'])
-        print('Line width:',self.settings['line_fitting_settings']['width'])
-
-    def print_res(self):
-        print('RESIDUAL PLOT SETTINGS')
-        print('S:',self.settings['residual_plot_settings']['s'])
+    def print_section(self, type):
+        print(type)
+        if len(self.settings[type]) == 0:
+            print ('No settings')
+        else:
+            for setting in self.settings[type]:
+                print(setting,'-',self.get(type,setting))
 
     def print_all(self):
         print('\nHere are the current settings for the program:')
         print()
-        self.print_io()
+        self.print_section('Input/Output Settings')
         print()
-        self.print_gen()
+        self.print_section('General Settings')
         print()
-        self.print_vis()
+        self.print_section('Histogram Visual Settings')
         print()
-        self.print_hist()
+        self.print_section('Histogram Generation Settings')
         print()
-        self.print_fit()
+        self.print_section('Line Fitting Settings')
         print()
-        self.print_res()
+        self.print_section('Residual Plot Settings')
