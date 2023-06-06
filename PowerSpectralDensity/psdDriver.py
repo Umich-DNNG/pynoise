5fa76064ca1b20d29238540ab1b3e5678d8d1d10
Vincent Weng <vincentweng@Vincents-MacBook-Pro.local>
2023-06-06 10:50:02 -0400

    added PSD to driver
    Former-commit-id: f6a6056abd4c9ab20633a3fbe6cafba24736fa66
    Former-commit-id: 06181dc56fa53bcb923718bace627876cf0765e8

--- PowerSpectralDensity/psdDriver.py
+++ PowerSpectralDensity/psdDriver.py
@@ -9,6 +9,7 @@ import numpy as np                    # For processing data
 import matplotlib.pyplot as plt       # For plotting data summaries
 from scipy.optimize import curve_fit
 from scipy import signal
+import power_spectral_density_scripts as psd
 
 def main():
     selection = 'blank'
@@ -24,10 +25,26 @@ def main():
             case 'm':
                 print()
                 print('Running the entire power spectral density analysis...')
+
+                file_path = "/Users/vincentweng/Documents/PyNoise/PowerSpectralDensity/sample_data/stilbene_2in_CROCUS_20cm_offset_east.txt"
+                values = np.loadtxt(file_path, usecols=(0,3), max_rows=2000000, dtype=float)
+
+                PowSpecDens_settings = {'dwell time': 2e6, 'meas time range': [150e9,1e12]}
+                
+                psd.conduct_APSD(list_data_array=values, 
+                                 PowSpecDens_settings=PowSpecDens_settings, 
+                                 leg_label="TEST", 
+                                 plt_title = "Power Spectral Density Graph", 
+                                 clean_pulses_switch=1)
             case 'p':
                 print()
                 print('Plotting the data...')
             case 'f':
                 print()
                 print('Fitting the data...')
+
+
+# Tells the program what function to start if this is the main program being run (TO BE DELETED)
+if __name__ == "__main__":
+    main()
                 
