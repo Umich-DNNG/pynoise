cbebf99a8adb5fe158543a2203ee3e4249971efa
maxwten <maxwten@umich.edu>
2023-06-22 10:09:38 -0400

    Initial settings editor + importing settings working
    Former-commit-id: 96fee9b22a88236f4a4eab4a4b8dba45aaba4438
    Former-commit-id: fac65f2f429452985769e35348a0826bd826b17f

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import gui
 
 def main():
     try:
-        sys.argv.index('--gui')
+        sys.argv.index('-g')
         gui.startup()
     except ValueError:
         try:
-            sys.argv.index('-g')
+            sys.argv.index('--gui')
             gui.startup()
         except ValueError:
             drv.main()
