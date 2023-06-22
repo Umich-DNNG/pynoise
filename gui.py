4152948d5eec911230b6aec133d68b226e0019af
maxwten <maxwten@umich.edu>
2023-06-22 11:35:59 -0400

    Working settings menus and psd runner
    Former-commit-id: 7f68feed432c94dad7a191435bf394dce30b3bf6
    Former-commit-id: 3b898ca88e576689d52a8b395cade4a1de806367

--- gui.py
+++ gui.py
@@ -3,10 +3,67 @@ from tkinter import ttk
 import tkinter as tk
 import settings as set
 import os
+import numpy as np
+from PowerSpectralDensity import PSD
 
 window = None
 parameters = None
 response = None
+
+def conduct_PSD():
+    global parameters
+
+    file_path = parameters.settings['Input/Output Settings']['Input file/folder']
+
+    values = np.loadtxt(file_path, usecols=(0,3), max_rows=2000000, dtype=float)
+
+    PSD_Object = PSD.PowerSpectralDensity(list_data_array=values, 
+                                        leg_label="TEST", 
+                                        clean_pulses_switch=parameters.settings['PSD Settings']['Clean pulses switch'], 
+                                        dwell_time=parameters.settings['PSD Settings']['Dwell time'], 
+                                        meas_time_range=parameters.settings['PSD Settings']['Meas time range'])
+    
+    PSD_Object.conduct_APSD(show_plot=parameters.settings['General Settings']['Show plots'], 
+                            save_fig=parameters.settings['General Settings']['Save figures'],
+                            save_dir=parameters.settings['Input/Output Settings']['Save directory'])
+
+def printType(value):
+    if isinstance(value, list):
+        response ='['
+        for entry in value:
+            response += printType(entry) + ', '
+        response = response[0:len(response)-2] + ']'
+        return response
+    if isinstance(value, bool):
+        if value:
+            return 'True'
+        else:
+            return 'False'
+    elif isinstance(value, float) and (value > 1000 or value < -1000 or (value < 0.01 and value > -0.01 and value != 0)):
+        return "{:e}".format(value)
+    else:
+        return str(value)
+
+def saveType(value: str):
+    if value[0] == '[':
+        value = value[1:len(value)-1]
+        response = []
+        while value.find(', ') != -1:
+            response.append(saveType(value[0:value.find(', ')]))
+            value=value[value.find(', ')+2:]
+        response.append(saveType(value))
+        return response
+    elif value == 'True':
+        return True
+    elif value == 'False':
+        return False
+    elif value.isnumeric():
+        return int(value)
+    try:
+        response = float(value)
+        return response
+    except ValueError:
+        return value
 
 def prompt(message, title, function, prev):
     global window, response
@@ -47,67 +104,7 @@ def error(message):
                ).grid(column=0,row=1)
     window.mainloop()
 
-def edit(append):
-    print('TODO!!!')
-
-def editor_menu(append):
-    global window, parameters
-    groupNum=0
-    curTop=0
-    curMax = 0
-    inputs=[]
-    for item in window.winfo_children():
-        item.destroy()
-    if append:
-        window.title('Append Settings')
-    else:
-        window.title('View/Edit Current Settings')
-    for group in parameters.settings:
-        if (groupNum*3) % 9 == 0:
-            curTop += curMax
-            curMax = 0
-        ttk.Label(window,
-                  name=group[0:group.find(' Settings')].lower(),
-                  text=group[0:group.find(' Settings')] + ':'
-                  ).grid(column=(groupNum*3) % 9,row=curTop)
-        setNum=1
-        for setting in parameters.settings[group]:
-            inputs.append(tk.StringVar())
-            ttk.Label(window,
-                  name=(group + ' ' + setting).lower(),
-                  text=setting + ':'
-                  ).grid(column=(groupNum*3+1) % 9,row=curTop+setNum)
-            tk.Entry(window,
-                  name=(group + ' ' + setting + ' value').lower(),
-                  textvariable=inputs[len(inputs)-1]
-                  ).grid(column=(groupNum*3+2) % 9,row=curTop+setNum)
-            if isinstance(parameters.settings[group][setting], bool):
-                if parameters.settings[group][setting]:
-                    window.children[(group + ' ' + setting + ' value').lower()].insert(0,'True')
-                else:
-                    window.children[(group + ' ' + setting + ' value').lower()].insert(0,'False')
-            elif isinstance(parameters.settings[group][setting], float) and (parameters.settings[group][setting] > 1000 or parameters.settings[group][setting] < 0.01):
-                window.children[(group + ' ' + setting + ' value').lower()].insert(0,"{:e}".format(parameters.settings[group][setting]))
-            else:
-                window.children[(group + ' ' + setting + ' value').lower()].insert(0,parameters.settings[group][setting])
-            setNum += 1
-        if setNum > curMax:
-            curMax = setNum
-        groupNum += 1
-    curTop += curMax
-    ttk.Button(window,
-                name='save',
-                text='Save changes',
-                command=lambda: edit(append)
-                ).grid(column=0 % 9,row=curTop)
-    ttk.Button(window,
-                  name='cancel',
-                  text='Cancel changes',
-                  command=setMenu
-                  ).grid(column=0 % 9,row=curTop+1)
-
-
-def setMenu():
+def setMenu(prev):
     global window
     for item in window.winfo_children():
         item.destroy()
@@ -119,23 +116,72 @@ def setMenu():
     ttk.Button(window,
               name='current',
               text='Current Settings',
-              command=lambda: editor_menu(False)
+              command=lambda: editor_menu(prev)
               ).grid(column=0,row=1)
     ttk.Button(window,
               name='import',
               text='Import settings file',
-              command=lambda: download_menu(setMenu, setMenu)
+              command=lambda: download_menu(lambda: setMenu(prev), setMenu, prev)
               ).grid(column=0,row=2)
     ttk.Button(window,
-              name='append',
-              text='Append settings',
-              command=lambda: editor_menu(True)
-              ).grid(column=0,row=3)
-    ttk.Button(window,
               name='return',
-              text='Return to main menu',
-              command=main
-              ).grid(column=0,row=4)
+              text='Return to previous menu',
+              command=prev
+              ).grid(column=0,row=3)
+
+def edit(inputs, prev):
+    global parameters
+    for group in inputs:
+        for setting in inputs[group]:
+            parameters.settings[group][setting] = saveType(inputs[group][setting].get())
+    setMenu(prev)
+
+def editor_menu(prev):
+    global window, parameters
+    groupNum=0
+    curTop=0
+    curMax = 0
+    inputs={}
+    for item in window.winfo_children():
+        item.destroy()
+    window.title('View/Edit Current Settings')
+    for group in parameters.settings:
+        if (groupNum*3) % 9 == 0:
+            curTop += curMax
+            curMax = 0
+        ttk.Label(window,
+                  name=group[0:group.find(' Settings')].lower(),
+                  text=group[0:group.find(' Settings')] + ':'
+                  ).grid(column=(groupNum*3) % 9,row=curTop)
+        setNum=1
+        for setting in parameters.settings[group]:
+            if inputs.get(group) == None:
+                inputs[group] = {}
+            inputs[group][setting]=tk.StringVar()
+            ttk.Label(window,
+                  name=(group + ' ' + setting).lower(),
+                  text=setting + ':'
+                  ).grid(column=(groupNum*3+1) % 9,row=curTop+setNum)
+            tk.Entry(window,
+                  name=(group + ' ' + setting + ' value').lower(),
+                  textvariable=inputs[group][setting]
+                  ).grid(column=(groupNum*3+2) % 9,row=curTop+setNum)
+            window.children[(group + ' ' + setting + ' value').lower()].insert(0,printType(parameters.settings[group][setting]))
+            setNum += 1
+        if setNum > curMax:
+            curMax = setNum
+        groupNum += 1
+    curTop += curMax
+    ttk.Button(window,
+                name='save',
+                text='Save changes',
+                command=lambda: edit(inputs, prev)
+                ).grid(column=0 % 9,row=curTop)
+    ttk.Button(window,
+                  name='cancel',
+                  text='Cancel changes',
+                  command=lambda: setMenu(prev)
+                  ).grid(column=0 % 9,row=curTop+1)
 
 def raMenu():
     global window
@@ -144,7 +190,7 @@ def raMenu():
     window.title('Rossi Alpha Analysis')
     ttk.Label(window,
               name='prompt',
-              text='What analysis would you like to perform?'
+              text='What would you like to do?'
               ).grid(column=0,row=0)
     ttk.Button(window,
               name='all',
@@ -165,7 +211,7 @@ def raMenu():
     ttk.Button(window,
               name='settings',
               text='Program settings',
-              command=setMenu
+              command=lambda: setMenu(raMenu)
               ).grid(column=0,row=5)
     ttk.Button(window,
               name='return',
@@ -180,17 +226,23 @@ def psdMenu():
     window.title('Power Spectral Density Analysis')
     ttk.Label(window,
               name='prompt',
-              text='What analysis would you like to perform?'
+              text='What would you like to do?'
               ).grid(column=0,row=0)
     ttk.Button(window,
+              name='run',
+              text='Run analysis',
+              command=conduct_PSD
+              ).grid(column=0,row=1)
+    ttk.Button(window,
               name='settings',
-              text='Program settings'
-              ).grid(column=0,row=5)
+              text='Program settings',
+              command=lambda: setMenu(psdMenu)
+              ).grid(column=0,row=2)
     ttk.Button(window,
               name='return',
               text='Return to main menu',
               command=main
-              ).grid(column=0,row=6)
+              ).grid(column=0,row=3)
 
 def shutdown():
     global window
@@ -218,7 +270,7 @@ def default():
     parameters.read(path)
     main()
 
-def download_menu(prev, to):
+def download_menu(prev, to, param):
     global window
     for item in window.winfo_children():
         item.destroy()
@@ -230,12 +282,12 @@ def download_menu(prev, to):
     ttk.Button(window,
                name='overwrite',
                text='Overwrite entire settings',
-               command=lambda: prompt('Enter a settings file (no .json extension):','Overwrite Settings',lambda: download(False, to),lambda: download_menu(prev, to))
+               command=lambda: prompt('Enter a settings file (no .json extension):','Overwrite Settings',lambda: download(False, to, param),lambda: download_menu(prev, to, param))
                ).grid(column=0,row=1)
     ttk.Button(window,
                name='append',
                text='Append settings to default',
-               command=lambda: prompt('Enter a settings file (no .json extension):','Append Settings to Default',lambda: download(True, to),lambda: download_menu(prev, to))
+               command=lambda: prompt('Enter a settings file (no .json extension):','Append Settings to Default',lambda: download(True, to, param),lambda: download_menu(prev, to, param))
                ).grid(column=0,row=2)
     ttk.Button(window,
                name='cancel',
@@ -243,7 +295,7 @@ def download_menu(prev, to):
                command=prev
                ).grid(column=0,row=3)
 
-def download(append, prev):
+def download(append, prev, param):
     global window, response, parameters
     file = response.get() + '.json'
     if os.path.isfile(os.path.abspath(file)):
@@ -252,7 +304,10 @@ def download(append, prev):
             parameters.append(os.path.abspath(file))
         else:
             parameters.read(os.path.abspath(file))
-        prev()
+        if param != None:
+            prev(param)
+        else:
+            prev()
     else:
         error('ERROR: ' + file + ' does not exist in the given directory.\n\n'
             + 'Make sure that your settings file is named correctly, '
@@ -275,12 +330,13 @@ def main():
                ).grid(column=0,row=1)
     ttk.Button(window,
                name='power_spectral_density',
-               text='Run Power Spectral Density analysis'
+               text='Run Power Spectral Density analysis',
+               command=psdMenu
                ).grid(column=0,row=2)
     ttk.Button(window,
                name='settings',
                text='Edit the program settings',
-               command=setMenu
+               command=lambda: setMenu(main)
                ).grid(column=0,row=3)
     ttk.Button(window,
                name='quit',
@@ -313,5 +369,5 @@ def startup():
             text='Would you like to use the default settings or import another .json file?'
             ).grid(column=0, row=1)
     ttk.Button(window, name='default', text="Default", command=default).grid(column=0, row=2)
-    ttk.Button(window, name='import', text="Import Settings", command=lambda:download_menu(startup, main)).grid(column=0, row=3)
+    ttk.Button(window, name='import', text="Import Settings", command=lambda:download_menu(startup, main, None)).grid(column=0, row=3)
     window.mainloop()
