80ac21ef04a2c8f0f58384ce00d00ae17df06432
katemcg <katemcg@umich.edu>
2023-06-26 10:30:27 -0500

    analyze all changes to check if input file is .lmx
    Former-commit-id: 81118010d260fd6a2b340d1b1cefb7a066460a45
    Former-commit-id: 4a998a4836112c1dd333408e472fc15c0d8eaa89

--- RossiAlpha/analyzeAll.py
+++ RossiAlpha/analyzeAll.py
@@ -12,6 +12,7 @@ from .timeDifs import timeDifCalcs
 from .plots import RossiHistogram
 from .fitting import RossiHistogramFit
 import matplotlib.pyplot as plt
+from lmxReader import *
 # sns.set(rc={"figure.dpi": 350, "savefig.dpi": 350})
 # sns.set_style("ticks")
 # sns.set_context("talk", font_scale=0.8)
@@ -22,15 +23,23 @@ def analyzeAllType1(settings: dict):
   
     
     filePath = settings['Input/Output Settings']['Input file/folder']
-    if settings['Input/Output Settings'].get('Data Column') is not None:
-        listData = np.loadtxt(filePath,delimiter=" ", usecols=(settings['Input/Output Settings']['Data Column']))
-    else:
-        listData = np.loadtxt(filePath)
+
+    if filePath.endswith(".txt"):
+        if settings['Input/Output Settings'].get('Data Column') is not None:
+            listData = np.loadtxt(filePath,delimiter=" ", usecols=(settings['Input/Output Settings']['Data Column']))
+        else:
+            listData = np.loadtxt(filePath)
     # sorting timestamps to be fed into calculate_time_differences()
+    elif filePath.endswith(".lmx"):
+        print("this is an lmx file. we are currently in testing stages for analyzing this file type.")
+        listData = np.array(readLMXFile(filePath))
+
+    
     
     if settings['General Settings']["Sort data"]:
         listDataSorted = np.sort(listData)
-    
+    else:
+        listDataSorted = listData
     # applying time differences function
     thisTimeDifCalc = timeDifCalcs(listDataSorted, settings['RossiAlpha Settings']['Histogram Generation Settings']["Reset time"],  settings['RossiAlpha Settings']["Time difference method"])
 
