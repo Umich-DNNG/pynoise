9a8e4d4647dd610fdef742566a8189e2393eae5a
katemcg <katemcg@umich.edu>
2023-06-29 11:10:14 -0500

    Cleaning up Plots.py to be used as python package
    Former-commit-id: 66d5399a3f439488f2ccd1a50652c31cd73dc3eb
    Former-commit-id: f73777a273e0f55d4541881f822e92cdf413e3ce

--- RossiAlpha/plots.py
+++ RossiAlpha/plots.py
@@ -5,7 +5,7 @@ import os
 plt.ioff()
 
 class RossiHistogram:
-    def __init__(self, reset_time: int, bin_width: int, plot_opts: dict, save_dir:str= None):
+    def __init__(self,  time_diffs = None, bin_width: int = None, reset_time: int = None):
 
         '''
         Description:
@@ -19,11 +19,14 @@ class RossiHistogram:
         Outputs: 
             - Plot() object
         '''
-
+        #Data Variables
+        self.time_diffs = time_diffs
+        self.reset_time = reset_time
+        self.bin_width = bin_width
         # Plotting options
-        self.options = plot_opts
-        self.save_dir = save_dir
-
+        self.options = None
+        self.save_dir = None
+        
         # Required parameters
         self.reset_time = reset_time
         self.bin_width = bin_width
@@ -34,7 +37,8 @@ class RossiHistogram:
         # Parameters set once plot(time_diffs) is called
         self.counts, self.bin_edges, self.bin_centers = None, None, None
 
-    def plot(self, time_diffs, save_fig: bool, show_plot: bool):
+    def plot(self, plot_opts: dict = None, save_fig: bool = True, show_plot: bool = True, save_dir:str= None ):
+        
         '''
         Creating histogram from an array of time differences and plotting it.
         Saving and showing the plot can be turned on or off.
@@ -48,12 +52,17 @@ class RossiHistogram:
             - bin_centers (adjusted bin centers for visual plotting)
             - bin_edges (The edges of the bins are mentioned as a parameter)
         '''
-  
+        self.options = plot_opts
+        self.save_dir = save_dir
+
+        if (self.reset_time == None) :
+            self.reset_time = np.max(self.time_diffs)
+
         # Calculating the number of bins
         num_bins = int(self.reset_time / self.bin_width)
 
         # Generating histogram
-        counts, bin_edges = np.histogram(time_diffs, bins=num_bins, range=[0, self.reset_time])
+        counts, bin_edges = np.histogram(self.time_diffs, bins=num_bins, range=[0, self.reset_time])
 
         # Adjusting the bin centers
         bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
@@ -94,12 +103,24 @@ class RossiHistogram:
 
         return counts, bin_centers, bin_edges
     
-    def plotFromHist(self, counts, bin_centers, bin_edges, show_plot: bool, save_fig: bool):
+    #This function is to initialize the RossiHistogram if we used the combined time_diffs and hist function
+    def initFromHist(self, counts, bin_centers, bin_edges):
+        self.counts = counts
+        self.bin_centers = bin_centers
+        self.bin_edges = bin_edges
+
+    def plotFromHist(self, plot_opts: dict = None, save_fig: bool = True, show_plot: bool = True, save_dir:str= None):
+        self.options = plot_opts
+        if not save_dir:
+            self.save_dir = save_dir
+        self.show_plot = show_plot
+
+        
         if save_fig == True:
 
             # Plotting
             plt.figure()
-            plt.bar(bin_centers, counts, width=0.8 * (bin_centers[1] - bin_centers[0]), **self.options)
+            plt.bar(self.bin_centers, self.counts, width=0.8 * (self.bin_centers[1] - self.bin_centers[0]), **self.options)
 
             plt.xlabel(self.x_axis)
             plt.ylabel(self.y_axis)
@@ -115,7 +136,7 @@ class RossiHistogram:
             # Plotting
             if not save_fig:
                 plt.figure()
-            plt.bar(bin_centers, counts, width=0.8 * (bin_centers[1] - bin_centers[0]), **self.options)
+            plt.bar(self.bin_centers, self.counts, width=0.8 * (self.bin_centers[1] - self.bin_centers[0]), **self.options)
 
             plt.xlabel(self.x_axis)
             plt.ylabel(self.y_axis)
@@ -123,7 +144,4 @@ class RossiHistogram:
             
             plt.show()
 
-        #Set the counts, bin_centers, and bin_edges of the object
-        self.counts = counts
-        self.bin_centers = bin_centers
-        self.bin_edges = bin_edges
+        
