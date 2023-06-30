c66395add02fd2acb28f073d203b5852025b11cd
katemcg <katemcg@umich.edu>
2023-06-30 12:19:14 -0500

    Issue #59 Integration of events class into analyzingFolders and timeDifs
    Former-commit-id: 7e63612175164a33edc8bb80756517246a0a7f94
    Former-commit-id: 0d569b5bfacd2c263519eb3bf8ce057712759f58

--- RossiAlpha/timeDifs.py
+++ RossiAlpha/timeDifs.py
@@ -1,18 +1,19 @@
 import numpy as np  # For processing data
 import matplotlib.pyplot as plt
 from . import plots as plt
+from Event import Event
 
 class timeDifCalcs:
     
-    def __init__(self, time_data: list[float], reset_time: float = None, method: str = 'any_and_all', digital_delay: int = None, channels = None):
-        # Store the raw time measurements.
-        self.time_vector = time_data 
+    def __init__(self, events, reset_time: float = None, method: str = 'any_and_all', digital_delay: int = None):
+        # Store events as a list.
+        self.events = events
         # If a reset time is given, use it.
         if reset_time != None:
             self.reset_time = float(reset_time)
         # Otherwise, generate one.
         else:
-            self.reset_time = max(time_data) - min(time_data)
+            self.reset_time = events[-1].time - events[0].time
         # Store the method of analysis.
         self.method = method
         # For considering digital delay.
@@ -24,12 +25,47 @@ class timeDifCalcs:
             # Otherwise, use the given value.
             else:
                 self.digital_delay = digital_delay
-        # If using channels, make a variable for it.
-        if self.method != "any_and_all":
-            self.channels = channels
+        
         # Initialize the blank time differences.
         self.timeDifs = None
+        
+
+
+
+    def calculateTimeDifsFromEvents(self):
+         # reset_time = float(Rossi_alpha_settings["reset time"])
+        time_diffs = np.array([])
+        n = len(self.events)
+        i = 0
+        # iterate from 0 through the whole time vector
+        while i < len(self.events):
+            ch_bank = set()
+            # iterate through the rest of the vector starting 1 after i
+
+            for j in range(i + 1, n):
+
+                # if we get outside the reset_time range, break to the next iteratiion of i
+                if self.events[j].time - self.events[i].time > self.reset_time:
+                    break
+                # if the method is any and all, there are no additional conditions, but if any other method, check that the channels are diff
+                if((self.method == 'any_and_all') or self.events[j].channel != self.events[i].channel):
+                    # if the method checks for repeats, check that it is not in the channels bank, otherwise we can add the time_diff
+                    if(self.method == 'any_and_all' or self.method == 'any_and_all cross_correlations' or self.events[j].channel not in ch_bank):
+                        time_diffs = np.append(time_diffs,(self.events[j].time - self.events[i].time))
+                    elif(self.method == 'any_and_all cross_correlations no_repeat digital_delay'):
+                        # add the digital delay if digital delay is on
+                        stamped_time = self.events[i].time
+                        while self.events[i].time < stamped_time + self.digital_delay:
+                           i = i + 1
+                    # add the current channel to the channels set if considering channels
+                    if(self.method != "any_and_all"):
+                        ch_bank.add(self.events[j].channel)
+            i = i + 1
+
+        self.timeDifs = time_diffs
+        return self.timeDifs
     
+
     def calculate_time_differences(self):
         '''can be called on a timeDifCalcs object and returns the array of time differences used for constructing a histogram based on the appropriate method
         
