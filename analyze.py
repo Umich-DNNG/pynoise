45144fb4fb0ae444ec18af5a2612e66e37cee189
maxwten <maxwten@umich.edu>
2023-07-14 09:53:11 -0400

    Integrated histogram visual settings into FeynmanY
    Former-commit-id: e3a6ef2104d4bb21ed898c9b02e3764d73f1e549
    Former-commit-id: 7d09afd9a776089354d38240d44c57ce0f151772

--- analyze.py
+++ analyze.py
@@ -25,11 +25,11 @@ class Analyzer:
         self.input: str = None
         self.method: str = None
 
-    def runFeynmanY(self, io: dict, fy: dict, show: bool, save: bool):
+    def runFeynmanY(self, io: dict, fy: dict, show: bool, save: bool, hvs: dict):
         data = evt.createEventsListFromTxtFile(io['Input file/folder'], io['Time column'], io['Channels column'])
         data.sort(key=lambda Event: Event.time)
         points = rc.randomCounts(data, fy['Tau'])
-        rc.FeynmanY_histogram(points, fy['Plot scale'], show, save, io['Save directory'])
+        rc.FeynmanY_histogram(points, fy['Plot scale'], show, save, io['Save directory'], hvs)
 
     def conductCohnAlpha(self, input: str, output: str, show: bool, save: bool, caGen: dict, caVis: dict):
 
