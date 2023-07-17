Fixed Y formula
Former-commit-id: 8bf08738d05f28ffa3a6664a494f4d21efb0b94f
Former-commit-id: e3841535c5f3b905dc8911c69d92d2f33c4443f1

FeynmanY/feynman.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import os
 
 def YFit(gamma, alpha, tau):
-    return gamma*(1-(1-np.exp(alpha*tau))/(alpha*tau))
+    return gamma*(1+(1-np.exp(alpha*tau))/(alpha*tau))
 
 def randomCounts(triggers: list[evt.Event], tau: int):
 