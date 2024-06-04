f59aa427dada626f3829c6de90019b9bf28755cd
Tiffany <tiffzhou@umich.edu>
2024-06-04 14:33:36 -0400

    Fix typo with time units setting
    "ns" was originally "ms" so the code did not accept ns as a valid time unit

--- settings.py
+++ settings.py
@@ -202,7 +202,7 @@ class Settings:
                 inputUnits = 1e-9
             case 'nanoseconds':
                 inputUnits = 1e-9
-            case 'ms':
+            case 'ns':
                 inputUnits = 1e-9
             case 'pico seconds':
                 inputUnits = 1e-12
@@ -243,7 +243,7 @@ class Settings:
                 outputUnits = 1e-9
             case 'nanoseconds':
                 outputUnits = 1e-9
-            case 'ms':
+            case 'ns':
                 outputUnits = 1e-9
             case 'pico seconds':
                 outputUnits = 1e-12
