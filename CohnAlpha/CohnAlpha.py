bf57cafd623c66073773baed24957fcbe42fbe99
Michael Yang <yangmi@umich.edu>
2024-06-20 15:11:26 -0400

    Modified code structure, placed helper functions inside of own file

--- CohnAlpha/CohnAlpha.py
+++ CohnAlpha/CohnAlpha.py
@@ -1,12 +1,11 @@
+from CohnAlpha import CohnAlphaHelper as helper
 import numpy as np                     # For processing data
 import matplotlib.pyplot as pyplot     # For plotting data summaries
 from scipy.optimize import curve_fit   # For fitting the curve
 from scipy import signal               # For welch (fourier transform)
-from pathlib import Path               # Currently strings for pathnames are not working, attempting to use pathlib/Path
-import datastream as ds
+from pathlib import Path               # path manipulation
+import datastream as ds                # for importing/exporting analysis
 import os                              # For saving figures
-import math                            # To compare floats and assign correct time units string
-#import h5py                            # For saving data in .hdf5 format                       
 
 
 # ------------ Power Spectral Density Fitting Function ----------------------------------------------
@@ -59,280 +58,6 @@ class CohnAlpha:
         
         self.print("Finished reading input file/folder data")
 
-    def conduct_CPSD(self,
-                    show_plot: bool = True,
-                    save_fig: bool = True,
-                    save_dir: str = './',
-                    caSet: dict = {},
-                    sps: dict = {},
-                    lfs: dict = {},
-                    scatter_opt: dict = {}):
-        
-        # Annotation Parameters
-        self.annotate_font_weight = caSet['Annotation Font Weight']
-        self.annotate_color = caSet['Annotation Color']
-        self.annotate_background_color = caSet['Annotation Background Color']
-
-        # Making count of bins over time histogram
-        count_bins = np.diff(self.meas_time_range) / self.dwell_time
-
-        counts_time_hist = np.zeros([2,int(count_bins)])
-
-        index = self.list_data_array[:,0]!=0
-
-        times = self.list_data_array[index, 0]
-
-        N, _ = np.histogram(
-                 times,
-                 bins=int(count_bins),
-                 range=self.meas_time_range
-                 )
-        counts_time_hist[0,:] = N
-
-        list_data_array = np.loadtxt(caSet['Second Input file'])
-        index = list_data_array[:,0]!=0
-
-        N, _ = np.histogram(list_data_array,
-                            bins=int(count_bins),
-                            range=self.meas_time_range)
-        
-        counts_time_hist[1,:] = N
-        
-        # counts_time_hist = np.zeros([2,int(count_bins)])
-        ''' if np.size(channels) == 2:
-            i=0
-            for ch in channels:
-                
-                if self.clean_pulses_switch == 1:
-                    indices = (self.list_data_array[:,-1]==1)
-
-                times = self.list_data_array[indices, 0]
-
-                N, _ = np.histogram(
-                    times,
-                    bins=int(count_bins),
-                    range=self.meas_time_range
-                    )
-                counts_time_hist[i,:] = N
-                i+=1
-        else:
-            print("Must specify two and only two channels to cross-correlate.")
-        '''
-
-        timeline = np.linspace(start=self.meas_time_range[0], 
-                            stop=self.meas_time_range[1],
-                            num=int(count_bins))/1e9
-
-        '''fig1, ax1 = pyplot.subplots()
-        ax1.plot(timeline, counts_time_hist, '.', label="TBD")
-        ax1.plot(timeline, counts_time_hist2, '.', label="TBD")
-
-        ax1.set_ylabel('Counts')
-        ax1.set_xlabel('Time(s)')
-        ax1.legend()'''
-        
-        # Plot counts over time histogram (ensure constant or near constant)
-        i=0
-        for ch in [0,1]:
-            fig1, ax1 = pyplot.subplots()
-            ax1.plot(timeline, counts_time_hist[i,:], '.', label="TBD")
-
-            ax1.set_ylabel('Counts')
-            ax1.set_xlabel('Time(s)')
-            ax1.legend()
-            i+=1
-
-        fs = 1/(timeline[3]-timeline[2]) # Get frequency of counts samples
-
-        f, Pxy = signal.csd(
-            counts_time_hist[0,:], 
-            counts_time_hist[1,:], 
-            fs, 
-            nperseg=2**10, 
-            window='boxcar')
-        Pxy = np.abs(Pxy)
-
-        '''f1, Pxx1 = signal.welch(x=counts_time_hist[0,:], 
-                            fs=fs, 
-                            nperseg=2**12, 
-                            window='boxcar')
-        
-        f2, Pxx2 = signal.welch(x=counts_time_hist[1,:], 
-                            fs=fs, 
-                            nperseg=2**12, 
-                            window='boxcar')'''
-
-        # Apply welch windows and FFT to tapered windows, summation is smoothed FFT
-
-        # f, Pxx = lpsd(counts_time_hist, fs, window='boxcar')
-        # # Apply logarithmically spaced power spectral density
-
-        popt, pcov = curve_fit(CAFit, f[1:-2], Pxy[1:-2],
-                                        p0=[Pxy[2], 25, 0],
-                                        bounds=(0, np.inf),
-                                        maxfev=100000
-                                        )
-
-        '''popt1, pcov1 = curve_fit(CAFit, f1[1:-2], 
-                                        Pxx1[1:-2],
-                                        p0=[Pxx1[2], 25, 0.001],
-                                        bounds=(0, np.inf),
-                                        maxfev=100000
-                                        )
-        
-        popt2, pcov2 = curve_fit(CAFit, f2[1:-2], 
-                                        Pxx2[1:-2],
-                                        p0=[Pxx2[2], 25, 0.001],
-                                        bounds=(0, np.inf),
-                                        maxfev=100000
-                                        )'''
-        
-        fig2, ax2 = pyplot.subplots()
-        ax2.semilogx(f[1:-2], Pxy[1:-2], '.', **sps)
-        ax2.semilogx(f[1:-2], CAFit(f[1:-2], *popt), **lfs)
-        ymin, ymax = ax2.get_ylim()
-        dy = ymax-ymin
-        ax2.set_xlim([1, 200])
-        ax2.set_xlabel('Frequency(Hz)')
-        ax2.set_ylabel('PSD (V$^2$/Hz)')
-        # ax2.set_yscale('log')
-        alph_str = (r'$\alpha$ = (' +
-                str(np.around(popt[1]*2*np.pi, decimals=2))+
-                '$\pm$ '+
-                str(np.around(np.sqrt(pcov[1,1])*2*np.pi, decimals=2))
-                +') 1/s')
-        ax2.annotate(alph_str, xy=(1.5, ymin+0.1*dy), xytext=(1.5, ymin+0.1*dy),
-                    fontsize=16, fontweight='bold',
-                    color='black', backgroundcolor='white')
-        # ax2.text(1.5, ymin+0.1*dy, alph_str)
-
-        # ax2.set_title(pyplot_title)
-        ax2.legend(loc='upper right')
-
-        ax2.grid()
-
-        '''
-        # Plotting the auto-power-spectral-density distribution and fit
-        figauto1, (axauto1, axauto2) = pyplot.subplots(nrows=2, sharex=True, figsize=(8, 6), gridspec_kw={'height_ratios': [2, 1]})
-        figauto2, (axauto3, axauto4) = pyplot.subplots(nrows=2, sharex=True, figsize=(8, 6), gridspec_kw={'height_ratios': [2, 1]})
-
-        # Creating a plot with semilogarithmic (log-scale) x-axis 
-        axauto1.semilogx(f1[1:-2], Pxx1[1:-2], '.', **sps)
-        axauto1.semilogx(f1[1:-2], CAFit(f[1:-2], *popt1), **lfs)
-
-        axauto3.semilogx(f2[1:-2], Pxx2[1:-2], '.', **sps)
-        axauto3.semilogx(f2[1:-2], CAFit(f[1:-2], *popt2), **lfs)
-
-        # Setting minimum and maximum for y
-        ymin, ymax = ax1.get_ylim()
-        dy = ymax-ymin
-
-        # Constructing alpha string
-        alph_str1 = (r'$\alpha$ = (' +
-                    str(np.around(popt1[1]*2*np.pi, decimals=2)) + '$\pm$ '+ 
-                    str(np.around(pcov1[1,1]*2*np.pi, decimals=2)) + ') 1/s')
-        alph_str2 = (r'$\alpha$ = (' +
-                    str(np.around(popt2[1]*2*np.pi, decimals=2)) + '$\pm$ '+ 
-                    str(np.around(pcov2[1,1]*2*np.pi, decimals=2)) + ') 1/s')
-        
-         # Annotating the plots
-        axauto1.annotate(alph_str1, 
-                    xy=(1.5, ymin+0.1*dy), 
-                    xytext=(1.5, ymin+0.1*dy),
-                    fontsize=16, 
-                    fontweight=self.annotate_font_weight,
-                    color=self.annotate_color, 
-                    backgroundcolor=self.annotate_background_color)
-        axauto3.annotate(alph_str2, 
-                    xy=(1.5, ymin+0.1*dy), 
-                    xytext=(1.5, ymin+0.1*dy),
-                    fontsize=16, 
-                    fontweight=self.annotate_font_weight,
-                    color=self.annotate_color, 
-                    backgroundcolor=self.annotate_background_color)
-        
-        # Creating title and legend
-        axauto1.set_title('Cohn Alpha Graph')
-        axauto1.legend(loc='upper right')
-
-        axauto3.set_title('Cohn Alpha Graph')
-        axauto3.legend(loc='upper right')
-
-        # Creating axis titles
-        axauto1.set_xlim([1, 200])
-        axauto1.set_xlabel('Frequency(Hz)')
-        axauto1.set_ylabel('Intensity (V$^2$/Hz)')
-
-        axauto3.set_xlim([1, 200])
-        axauto3.set_xlabel('Frequency(Hz)')
-        axauto3.set_ylabel('Intensity (V$^2$/Hz)')
-
-        # Compute residuals
-        residuals1 = ((CAFit(f1[1:-2], *popt1) - Pxx1[1:-2]) / Pxx1[1:-2]) * 100
-        residuals2 = ((CAFit(f2[1:-2], *popt2) - Pxx2[1:-2]) / Pxx2[1:-2]) * 100
-
-        # Computing residuals and plot in bottom subplot
-        axauto2.scatter(f[1:-2], residuals1, **scatter_opt)  # Use f for residuals
-        axauto2.axhline(y=0, color='#162F65', linestyle='--')
-        axauto2.set_xlabel('Frequency(Hz)')
-        axauto2.set_ylabel('Percent difference (%)')
-
-        axauto4.scatter(f[1:-2], residuals2, **scatter_opt)  # Use f for residuals
-        axauto4.axhline(y=0, color='#162F65', linestyle='--')
-        axauto4.set_xlabel('Frequency(Hz)')
-        axauto4.set_ylabel('Percent difference (%)')
-        
-        # Saving figures (optional)
-        if save_fig:
-            fig1.tight_layout()
-            save_filename = os.path.join(save_dir, 'CohnAlpha')
-            fig1.savefig(save_filename, dpi=300, bbox_inches='tight')
-
-            fig2.tight_layout()
-            save_filename = os.path.join(save_dir, 'CohnAlpha')
-            fig2.savefig(save_filename, dpi=300, bbox_inches='tight')
-
-            figauto1.tight_layout()
-            save_filename = os.path.join(save_dir, 'CohnAlpha')
-            figauto1.savefig(save_filename, dpi=300, bbox_inches='tight')
-
-            figauto2.tight_layout()
-            save_filename = os.path.join(save_dir, 'CohnAlpha')
-            figauto2.savefig(save_filename, dpi=300, bbox_inches='tight')
-
-        # Showing plots (optional)
-        if show_plot:
-            pyplot.show()
-        '''
-        pyplot.show()
-
-        return f, Pxy, popt, pcov    
-
-    def conductCohnAlpha(self, settings: dict = {}):
-        
-        '''
-        Deprecated Function. Just call fitCohnAlpha() instead
-
-
-        Creating PSD plot from an array of data inputs.
-        Saving and showing the plot can be turned on or off.
-        Visual settings can also be adjusted.
-
-        Inputs:
-            - self: all the private variables in PowerSpectralDensity() object
-            - settings: the current user runtime settings
-
-        Outputs: 
-            - f (DESCRIPTION NEEDED)
-            - Pxx (DESCRIPTION NEEDED)
-            - popt (Optimal values for the parameters)
-            - pcov (DESCRIPTION NEEDED)
-        '''
-
-        return
-
-        # counts_time_hist = self.plotHistogram(settings)
-        # return self.welchApproximationFourierTransformation(settings, counts_time_hist)
 
     def plotCountsHistogram(self, settings: dict = {}):
 
@@ -459,7 +184,7 @@ class CohnAlpha:
             nperseg = 1 / (dwell_time * settings['CohnAlpha Settings']['Frequency Minimum'])
 
             # check to see if nperseg is a power of 2
-            if not checkPowerOfTwo(value=int(nperseg)):
+            if not helper.checkPowerOfTwo(value=int(nperseg)):
                 self.print('WARNING: calculated nperseg is not a power of 2')
 
             # first attempt to read in file
@@ -515,9 +240,26 @@ class CohnAlpha:
             - self: all the private variables in PowerSpectralDensity() object
             - settings: the current user runtime settings
             - welchResultDict: the dict that contains the result of Welch's Approximation
-
+            
         Output:
             - dict: contains the fitted curve
+
+        --------------------------------------------------------------------------------
+        Old docstring:
+        Creating PSD plot from an array of data inputs.
+        Saving and showing the plot can be turned on or off.
+        Visual settings can also be adjusted.
+
+        Inputs:
+            - self: all the private variables in PowerSpectralDensity() object
+            - settings: the current user runtime settings
+
+        Outputs: 
+            - f (DESCRIPTION NEEDED)
+            - Pxx (DESCRIPTION NEEDED)
+            - popt (Optimal values for the parameters)
+            - pcov (DESCRIPTION NEEDED)
+    
         '''
 
         self.print('\nFitting Power Spectral Density Curve...')
@@ -595,10 +337,7 @@ class CohnAlpha:
         # call curve_fit and everything inside the fit function
         # self.plot(x=f, y=Pxx, method='fit', settings=settings)
 
-        
-
-
-        
+       
         # Plotting the auto-power-spectral-density distribution and fit
         fig, ax = pyplot.subplots(nrows=2, sharex=True, figsize=(8, 6), gridspec_kw={'height_ratios': [2, 1]})
 
@@ -778,93 +517,54 @@ class CohnAlpha:
         pyplot.show()
         pyplot.close()
 
+        def fit(self, fig, ax, fitEquation, **kwargs):
 
-    def fit(self, fig, ax, fitEquation, **kwargs):
+            return
 
-        '''
-        Fitting Function; assumes plot() has been called with method='fit' 
-        Fits scatterplot with Power Spectral Density Curve
+            '''
+            Fitting Function; assumes plot() has been called with method='fit' 
+            Fits scatterplot with Power Spectral Density Curve
 
-        Inputs:
-            self: (TODO: ADD DESCRIPTION)
-            fig: the figure returned by pyplot.subplots()
-            ax: the axes returned by pyplot.subplots()
-            fitEquation: the equation that will be used by curve_fit() to fit the graph
-            kwargs: (TODO: ADD DESCRIPTION)
-        '''
-        'Percent difference (%)'
-        popt = []
-        popt.append(kwargs['optimal a'])
-        popt.append(kwargs['optimal alpha'])
-        popt.append(kwargs['optimal c'])
-        popt = np.array(popt,dtype=np.float64)
-        ax[0].semilogx(x[1:-2], CAFit(y[1:-2], *popt), **settings['Line Fitting Settings'])                
-        ax[0].legend(loc='upper right')
-        ax[1].scatter(x[1:-2], residuals, **settings['Scatter Plot Settings'])
-        ax[1].axhline(y=0, color='#162F65', linestyle='--')
-        ax[1].set_xlabel(xLabel)
-        ax[1].set_ylabel(y2Label)
+            Inputs:
+                self: (TODO: ADD DESCRIPTION)
+                fig: the figure returned by pyplot.subplots()
+                ax: the axes returned by pyplot.subplots()
+                fitEquation: the equation that will be used by curve_fit() to fit the graph
+                kwargs: (TODO: ADD DESCRIPTION)
+            '''
+            'Percent difference (%)'
+            popt = []
+            popt.append(kwargs['optimal a'])
+            popt.append(kwargs['optimal alpha'])
+            popt.append(kwargs['optimal c'])
+            popt = np.array(popt,dtype=np.float64)
+            ax[0].semilogx(x[1:-2], CAFit(y[1:-2], *popt), **settings['Line Fitting Settings'])                
+            ax[0].legend(loc='upper right')
+            ax[1].scatter(x[1:-2], residuals, **settings['Scatter Plot Settings'])
+            ax[1].axhline(y=0, color='#162F65', linestyle='--')
+            ax[1].set_xlabel(xLabel)
+            ax[1].set_ylabel(y2Label)
 
-        # NOTE: should be able to pass in alpha and uncertainty through **kwargs
-        # don't need below code
-        # alpha = np.around(popt[1]*2*np.pi, decimals=2)
-        # uncertainty = np.around(pcov[1,1]*2*np.pi, decimals=2)
-        alpha = kwargs['alpha']
-        uncertainty = kwargs['uncertainty']
-        self.print('alpha = ' + str(alpha) + ', uncertainty = ' + 
-            str(uncertainty))
-        
-        ymin, ymax = ax[0].get_ylim()
-        dy = ymax - ymin
-        alph_str = (r'$\alpha$ = (' +
-                    '{:.2e}'.format(alpha) + '$\pm$ ' +
-                    '{:.2e}'.format(uncertainty) + ') 1/s')
-        ax[0].annotate(alph_str,
-                        xy=(1.5, ymin+0.1*dy),
-                        xytext=(1.5, ymin+0.1*dy),
-                        fontsize=self.annotate_font_size,
-                        fontweight=self.annotate_font_weight,
-                        color=self.annotate_color,
-                        backgroundcolor=self.annotate_background_color)
+            # NOTE: should be able to pass in alpha and uncertainty through **kwargs
+            # don't need below code
+            # alpha = np.around(popt[1]*2*np.pi, decimals=2)
+            # uncertainty = np.around(pcov[1,1]*2*np.pi, decimals=2)
+            alpha = kwargs['alpha']
+            uncertainty = kwargs['uncertainty']
+            self.print('alpha = ' + str(alpha) + ', uncertainty = ' + 
+                str(uncertainty))
+            
+            ymin, ymax = ax[0].get_ylim()
+            dy = ymax - ymin
+            alph_str = (r'$\alpha$ = (' +
+                        '{:.2e}'.format(alpha) + '$\pm$ ' +
+                        '{:.2e}'.format(uncertainty) + ') 1/s')
+            ax[0].annotate(alph_str,
+                            xy=(1.5, ymin+0.1*dy),
+                            xytext=(1.5, ymin+0.1*dy),
+                            fontsize=self.annotate_font_size,
+                            fontweight=self.annotate_font_weight,
+                            color=self.annotate_color,
+                            backgroundcolor=self.annotate_background_color)
 
-
-# ---------------------------------------------------------------------------------------------------   
-
-# Helper Functions
-def convertTimeUnitsToStr(units):
-    # femtoseconds
-    if (math.isclose(a=units, b=1e-15, abs_tol=1e-15)):
-        return 'fs'
-    # picoseconds
-    if (math.isclose(a=units, b=1e-12, abs_tol=1e-12)):
-        return 'ps'    
-    # nano seconds
-    if (math.isclose(a=units, b=1e-9, abs_tol=1e-9)):
-        return 'ns'
-    # microseconds
-    if (math.isclose(a=units, b=1e-6, abs_tol=1e-6)):
-        return 'us'
-    # milliseconds
-    if (math.isclose(a=units, b=1e-3, abs_tol=1e-3)):
-        return 'ms'
-    # seconds
-    if (math.isclose(a=units, b=1, abs_tol=1)):
-        return 's'
-
-
-
-def checkPowerOfTwo(value):
-    '''Function to check if value is a power of 2 or not
-    Returns True if a power of 2, otherwise returns False
-    Note: make value an integer, otherwise cannot properly calculate
-    
-    Input:
-    - value (int): the value to be checked'''
-
-    while (value > 1):
-        value = value / 2
-
-    if value == 1:
-        return True
-    if value < 1:
-        return False
+# ---------------------------------------------------------------------------------------------------
---
+++ CohnAlpha/CohnAlphaHelper.py
@@ -0,0 +1,41 @@
+import math                            # To compare floats and assign correct time units string
+# import h5py                            # For saving data in .hdf5 format
+
+# Helper Functions
+def convertTimeUnitsToStr(units):
+    # femtoseconds
+    if (math.isclose(a=units, b=1e-15, abs_tol=1e-15)):
+        return 'fs'
+    # picoseconds
+    if (math.isclose(a=units, b=1e-12, abs_tol=1e-12)):
+        return 'ps'    
+    # nano seconds
+    if (math.isclose(a=units, b=1e-9, abs_tol=1e-9)):
+        return 'ns'
+    # microseconds
+    if (math.isclose(a=units, b=1e-6, abs_tol=1e-6)):
+        return 'us'
+    # milliseconds
+    if (math.isclose(a=units, b=1e-3, abs_tol=1e-3)):
+        return 'ms'
+    # seconds
+    if (math.isclose(a=units, b=1, abs_tol=1)):
+        return 's'
+
+
+
+def checkPowerOfTwo(value):
+    '''Function to check if value is a power of 2 or not
+    Returns True if a power of 2, otherwise returns False
+    Note: make value an integer, otherwise cannot properly calculate
+    
+    Input:
+    - value (int): the value to be checked'''
+
+    while (value > 1):
+        value = value / 2
+
+    if value == 1:
+        return True
+    if value < 1:
+        return False
